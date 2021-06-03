4af2e8eb2700f9a9a5b98ce2c5808ce75c9ec542
Jesse Smith <worldjesses@gmail.com>
2021-06-03 12:14:06 -0400

    Game/Gamesystem/Genre Created

--- BFGgameFinder.Data/Game.cs
+++ BFGgameFinder.Data/Game.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,22 @@ namespace BFGgameFinder.Data
 {
     public class Game
     {
+        [Key]
+        public int GameId { get; set; }
+
+        [Required]
+        public Guid GamerTag { get; set; }
+
+        [Required]
+        public string GameName { get; set; }
+
+        [Required]
+        public string Rating { get; set; }
+
+        public DateTimeOffset ReleaseDate { get; set; }
+
+        [ForeignKey(nameof(Genre))]
+        public int? GenreId { get; set; }
+        public virtual Genre Genre { get; set; }
     }
 }
