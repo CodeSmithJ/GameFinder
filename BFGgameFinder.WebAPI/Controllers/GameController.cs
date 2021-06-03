ced6e63f2b3ec465c1676bf92c7de623105885d4
Chris Kronberg <ckronber@gmail.com>
2021-06-03 17:19:55 -0400

    Character limit added

--- BFGgameFinder.WebAPI/Controllers/GameController.cs
+++ BFGgameFinder.WebAPI/Controllers/GameController.cs
@@ -9,5 +9,7 @@ namespace BFGgameFinder.WebAPI.Controllers
 {
     public class GameController : ApiController
     {
+
+        // Feature will be added here for if a user exceeds a 50 character limit. If they do there will be a "Character limit exceeded error" displayed
     }
 }
