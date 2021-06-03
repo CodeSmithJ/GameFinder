69efd45f51e3ff100f0143f5d9795c127c258efa
Raymond Sipes <raymondsipes21@gmail.com>
2021-06-03 17:37:40 -0400

    admin remove feature raymond

--- BFGgameFinder.WebAPI/Controllers/GameController.cs
+++ BFGgameFinder.WebAPI/Controllers/GameController.cs
@@ -9,5 +9,7 @@ namespace BFGgameFinder.WebAPI.Controllers
 {
     public class GameController : ApiController
     {
+        // Build feature for admin to remove games, return 200 Ok 
+        // if error return 400 "Game could not be removed"
     }
 }
