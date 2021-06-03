75eb173003a2b453410330c44aca380ff17fc3a2
Raymond Sipes <raymondsipes21@gmail.com>
2021-06-03 17:32:29 -0400

    Genre filter feature Raymond

--- BFGgameFinder.WebAPI/Controllers/GameController.cs
+++ BFGgameFinder.WebAPI/Controllers/GameController.cs
@@ -9,5 +9,7 @@ namespace BFGgameFinder.WebAPI.Controllers
 {
     public class GameController : ApiController
     {
+        // would add a feature to filter game out based on the users gerne input, should return users with title of games based on the genre. 
+        // if no games for genre will return error 400 saying "no game by that genre"
     }
 }
