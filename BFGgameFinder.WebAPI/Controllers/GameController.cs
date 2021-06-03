1ac8efdeafc4920bc910bab1063651a19f00460e
Chris Kronberg <ckronber@gmail.com>
2021-06-03 17:28:57 -0400

    Admin Update

--- BFGgameFinder.WebAPI/Controllers/GameController.cs
+++ BFGgameFinder.WebAPI/Controllers/GameController.cs
@@ -9,5 +9,7 @@ namespace BFGgameFinder.WebAPI.Controllers
 {
     public class GameController : ApiController
     {
+        //This is a feature for Admins to update games. This should update information on the game and return a 200 ok message. 
+        //If it is not updated correctly it will return a 400 error with a message "Game was not successfully added"
     }
 }
