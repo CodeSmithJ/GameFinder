b61daa03635061490838e238e51a221eb2ded680
Chris Kronberg <ckronber@gmail.com>
2021-06-03 17:23:55 -0400

    Most downloaded feature

--- BFGgameFinder.WebAPI/Controllers/GameController.cs
+++ BFGgameFinder.WebAPI/Controllers/GameController.cs
@@ -9,5 +9,7 @@ namespace BFGgameFinder.WebAPI.Controllers
 {
     public class GameController : ApiController
     {
+        //This feature adds a method for sending back games that have been downloaded the most.
+        //If this fails it will send back a game not found it will send back a "Game not found" error message.
     }
 }
