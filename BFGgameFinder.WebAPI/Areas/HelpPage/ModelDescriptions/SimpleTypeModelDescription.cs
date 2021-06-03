f8e35cfcbda09cde6d772fcd7ddc3fef786e19c6
Jesse Smith <worldjesses@gmail.com>
2021-06-03 10:49:47 -0400

    Initial Commit

---
+++ BFGgameFinder.WebAPI/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace BFGgameFinder.WebAPI.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ BFGgameFinder.WebAPI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BFGgameFinder.WebAPI.WebApiApplication" Language="C#" %>
