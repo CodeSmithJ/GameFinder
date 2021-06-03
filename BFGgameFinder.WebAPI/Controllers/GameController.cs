3113e63f7a560f892a6501890105b69e62bda155
Raymond Sipes <raymondsipes21@gmail.com>
2021-06-03 17:35:12 -0400

    community post feature Raymond

--- BFGgameFinder.WebAPI/Controllers/GameController.cs
+++ BFGgameFinder.WebAPI/Controllers/GameController.cs
@@ -9,5 +9,7 @@ namespace BFGgameFinder.WebAPI.Controllers
 {
     public class GameController : ApiController
     {
+        // Build a feature to show community posts related to game being viewed, Return 200 ok if community post are related to game
+        // error code 400  saying "No community post related to the game"
     }
 }
